a333c08e1fd7b54b9301b5ca70c6088d56a9c745
Anik-web-code <alianik11star@gmail.com>
2025-03-02 15:26:02 +0600

    Added Script

--- script.js
+++ script.js
@@ -15,3 +15,31 @@ function updateDateTime(){
 }
 
 updateDateTime();
+
+let taskAssigned = document.getElementById('task-Assigned');
+let taskCompleted = document.getElementById('task-completed');
+
+let assigned = parseInt(taskAssigned.innerText);
+let completed = parseInt(taskCompleted.innerText);
+
+function taskAdjustor(){
+    assigned--;
+    completed++;
+    taskAssigned.innerText = `${assigned}`;
+    taskCompleted.innerText = `${completed}`;
+
+}
+
+let button1 = document.getElementById('btn1');
+let button2 = document.getElementById('btn2');
+let button3 = document.getElementById('btn3');
+let button4 = document.getElementById('btn4');
+let button5 = document.getElementById('btn5');
+let button6 = document.getElementById('btn6');
+
+let task1 = document.getElementById('task-1');
+let task2 = document.getElementById('task-2');
+let task3 = document.getElementById('task-3');
+let task4 = document.getElementById('task-4');
+let task5 = document.getElementById('task-5');
+let task6 = document.getElementById('task-6');
